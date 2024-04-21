595523ebdeb1dba1aec7fb84acb633119bf10673
Bryce-Kaddouri-Dublin <bryce.kaddouri.dublin@gmail.com>
2024-04-21 20:17:50 +0100

    refactor notifcation list page by spliting in promotion / order notitifcarton

--- lib/src/feature/notification/data/model/notification_model.dart
+++ lib/src/feature/notification/data/model/notification_model.dart
@@ -6,26 +6,32 @@ class NotificationModel {
   final String? userId;
   final int? orderId;
   final DateTime? order_date;
+  final String type;
+  final String? photoUrl;
 
-  NotificationModel({
-    required this.id,
-    required this.title,
-    required this.body,
-    required this.createdAt,
-    required this.userId,
-    required this.orderId,
-    required this.order_date,
-  });
+  NotificationModel(
+      {required this.id,
+      required this.title,
+      required this.body,
+      required this.createdAt,
+      required this.userId,
+      required this.orderId,
+      required this.order_date,
+      required this.type,
+      this.photoUrl});
 
   factory NotificationModel.fromJson(Map<String, dynamic> json) {
     return NotificationModel(
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-      createdAt: DateTime.parse(json['created_at']),
-      userId: json['user_id'],
-      orderId: json['order_id'],
-      order_date: json['order_date'] != null ? DateTime.parse(json['order_date']) : null,
-    );
+        id: json['id'],
+        title: json['title'],
+        body: json['body'],
+        createdAt: DateTime.parse(json['created_at']),
+        userId: json['user_id'],
+        orderId: json['order_id'],
+        order_date: json['order_date'] != null
+            ? DateTime.parse(json['order_date'])
+            : null,
+        type: json['type'],
+        photoUrl: json['type'] == 'PROMOTION' ? json['photo_url'] : null);
   }
 }
--- lib/src/feature/notification/presentation/screen/notification_list_screen.dart
+++ lib/src/feature/notification/presentation/screen/notification_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:customer_app/src/core/helper/date_helper.dart';
 import 'package:customer_app/src/feature/notification/presentation/provider/notification_provider.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as mat;
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -20,36 +21,72 @@ class NotificationListScreen extends StatefulWidget {
   State<NotificationListScreen> createState() => _NotificationListScreenState();
 }
 
-class _NotificationListScreenState extends State<NotificationListScreen> {
-  List<NotificationByDate>? notificationByDateList;
+class _NotificationListScreenState extends State<NotificationListScreen>
+    with SingleTickerProviderStateMixin {
+  List<NotificationByDate> notificationByDateListOrder = [];
+  List<NotificationByDate> notificationByDateListPromotion = [];
   bool isLoading = false;
+  int tabIndex = 0;
+  late mat.TabController tabController;
+  ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    tabController = mat.TabController(length: 2, vsync: this);
     setState(() {
       isLoading = true;
     });
     context.read<NotificationProvider>().getNotifications().then((value) {
       if (value != null) {
-        List<NotificationByDate> notificationByDateListTemp = [];
-        for (NotificationModel notification in value) {
+        List<NotificationByDate> notificationByDateListTempOrder = [];
+        List<NotificationByDate> notificationByDateListTempPromotion = [];
+        List<NotificationModel> orderNotifications =
+            value.where((element) => element.type != "PROMOTION").toList();
+        List<NotificationModel> promotionNotification =
+            value.where((element) => element.type == "PROMOTION").toList();
+
+        for (var orderNotification in orderNotifications) {
           bool isExist = false;
-          for (NotificationByDate notificationByDate in notificationByDateListTemp!) {
-            if (DateHelper.isSameDay(notificationByDate.date, notification.createdAt)) {
-              notificationByDate.notifications.add(notification);
+          for (NotificationByDate notificationByDate
+              in notificationByDateListTempOrder) {
+            if (DateHelper.isSameDay(
+                notificationByDate.date, orderNotification.createdAt)) {
+              notificationByDate.notifications.add(orderNotification);
               isExist = true;
               break;
             }
           }
           if (!isExist) {
-            notificationByDateListTemp!.add(NotificationByDate(date: notification.createdAt.copyWith(hour: 0, minute: 0, second: 0), notifications: [notification]));
+            notificationByDateListTempOrder.add(NotificationByDate(
+                date: orderNotification.createdAt
+                    .copyWith(hour: 0, minute: 0, second: 0),
+                notifications: [orderNotification]));
           }
         }
-        print('notificationByDateListTemp');
-        print(notificationByDateListTemp);
+
+        for (var promotionNotification in promotionNotification) {
+          bool isExist = false;
+          for (NotificationByDate notificationByDate
+              in notificationByDateListTempPromotion) {
+            if (DateHelper.isSameDay(
+                notificationByDate.date, promotionNotification.createdAt)) {
+              notificationByDate.notifications.add(promotionNotification);
+              isExist = true;
+              break;
+            }
+          }
+          if (!isExist) {
+            notificationByDateListTempPromotion.add(NotificationByDate(
+                date: promotionNotification.createdAt
+                    .copyWith(hour: 0, minute: 0, second: 0),
+                notifications: [promotionNotification]));
+          }
+        }
+
         setState(() {
-          notificationByDateList = notificationByDateListTemp;
+          notificationByDateListOrder = notificationByDateListTempOrder;
+          notificationByDateListPromotion = notificationByDateListTempPromotion;
         });
       }
     }).whenComplete(() {
@@ -58,71 +95,196 @@ class _NotificationListScreenState extends State<NotificationListScreen> {
       });
       print('NotificationListScreen initState');
       print('notificationByDateList');
-      print(notificationByDateList);
+      print(notificationByDateListOrder);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.all(20),
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width,
-          minHeight: MediaQuery.of(context).size.height - 60,
-        ),
-        color: FluentTheme.of(context).navigationPaneTheme.overlayBackgroundColor,
-        child: isLoading
-            ? Center(
-                child: ProgressRing(),
-              )
-            : Column(
+    return Container(
+      padding: EdgeInsets.all(20),
+      constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width,
+        minHeight: MediaQuery.of(context).size.height - 60,
+      ),
+      color: FluentTheme.of(context).navigationPaneTheme.overlayBackgroundColor,
+      child: isLoading
+          ? Center(
+              child: ProgressRing(),
+            )
+          : Container(
+              child: Column(
                 children: [
-                  Button(
-                    onPressed: () async {
-                      context.go('/test');
-                    },
-                    child: Text('Refresh'),
-                  ),
-                  for (NotificationByDate notificationByDate in notificationByDateList!) ...[
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      margin: EdgeInsets.only(bottom: 10),
-                      child: Text(
-                        DateHelper.getFormattedDate(notificationByDate.date),
-                        style: FluentTheme.of(context).typography.subtitle,
+                  mat.TabBar(
+                    controller: tabController,
+                    tabs: [
+                      mat.Tab(
+                        child: Text(
+                            "Promotion (${notificationByDateListPromotion.fold(0, (previousValue, element) => previousValue + element.notifications.length)})"),
                       ),
-                    ),
-                    for (NotificationModel notification in notificationByDate.notifications) ...[
-                      Card(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                            children: [
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  notification.title ?? '',
-                                  style: FluentTheme.of(context).typography.subtitle,
-                                ),
-                              ),
-                              Container(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  notification.body ?? '',
-                                  style: FluentTheme.of(context).typography.body,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                      mat.Tab(
+                        child: Text(
+                            "Order (${notificationByDateListOrder.fold(0, (previousValue, element) => previousValue + element.notifications.length)})"),
                       ),
                     ],
-                  ],
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: mat.TabBarView(
+                        controller: tabController,
+                        children: [
+                          // promotion
+                          Container(
+                            child: ListView.builder(
+                              controller: scrollController,
+                              itemCount: notificationByDateListPromotion.length,
+                              itemBuilder: (context, index) {
+                                if (index == 0) {
+                                  return Container(
+                                    alignment: Alignment.centerLeft,
+                                    margin: EdgeInsets.only(bottom: 10),
+                                    child: Text(
+                                      DateHelper.getFormattedDate(
+                                          notificationByDateListPromotion[index]
+                                              .date),
+                                      style: FluentTheme.of(context)
+                                          .typography
+                                          .subtitle,
+                                    ),
+                                  );
+                                }
+
+                                return ListView.builder(
+                                    controller: scrollController,
+                                    shrinkWrap: true,
+                                    itemCount:
+                                        notificationByDateListPromotion[index]
+                                            .notifications
+                                            .length,
+                                    itemBuilder: (context, indexNotification) {
+                                      NotificationModel notification =
+                                          notificationByDateListPromotion[index]
+                                              .notifications[indexNotification];
+                                      return Card(
+                                        margin: EdgeInsets.only(bottom: 10),
+                                        child: ListTile(
+                                          leading: Container(
+                                            height: 50,
+                                            width: 50,
+                                            child: Image.network(
+                                              notification.photoUrl ?? '',
+                                            ),
+                                          ),
+                                          title: Container(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text(
+                                              notification.title ?? '',
+                                              style: FluentTheme.of(context)
+                                                  .typography
+                                                  .subtitle,
+                                            ),
+                                          ),
+                                          subtitle: Container(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text(
+                                              notification.body ?? '',
+                                              style: FluentTheme.of(context)
+                                                  .typography
+                                                  .body,
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    });
+                              },
+                            ),
+                          ),
+                          Container(
+                            child: ListView.builder(
+                              itemCount: notificationByDateListOrder.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  children: [
+                                    for (NotificationByDate notificationByDate
+                                        in notificationByDateListOrder) ...[
+                                      Container(
+                                        alignment: Alignment.centerLeft,
+                                        margin: EdgeInsets.only(bottom: 10),
+                                        child: Text(
+                                          DateHelper.getFormattedDate(
+                                              notificationByDate.date),
+                                          style: FluentTheme.of(context)
+                                              .typography
+                                              .subtitle,
+                                        ),
+                                      ),
+                                      for (NotificationModel notification
+                                          in notificationByDate
+                                              .notifications) ...[
+                                        Card(
+                                          padding: EdgeInsets.all(0),
+                                          margin: EdgeInsets.only(bottom: 10),
+                                          child: ListTile(
+                                            onPressed: () {
+                                              context.push(
+                                                  '/orders/${DateHelper.getFormattedDate(notification.order_date!)}/${notification.orderId}');
+                                            },
+                                            leading: Container(
+                                              height: 50,
+                                              width: 50,
+                                              decoration: BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  border: Border.all(width: 1)),
+                                              child: notification.type ==
+                                                      'INSERT'
+                                                  ? Icon(
+                                                      FluentIcons.product_list,
+                                                      size: 24,
+                                                    )
+                                                  : Icon(
+                                                      FluentIcons
+                                                          .product_release,
+                                                      size: 24,
+                                                    ),
+                                            ),
+                                            title: Container(
+                                              alignment: Alignment.centerLeft,
+                                              child: Text(
+                                                notification.title ?? '',
+                                                style: FluentTheme.of(context)
+                                                    .typography
+                                                    .subtitle,
+                                              ),
+                                            ),
+                                            subtitle: Container(
+                                              alignment: Alignment.centerLeft,
+                                              child: Text(
+                                                notification.body ?? '',
+                                                style: FluentTheme.of(context)
+                                                    .typography
+                                                    .body,
+                                              ),
+                                            ),
+                                            trailing:
+                                                Icon(FluentIcons.chevron_right),
+                                          ),
+                                        ),
+                                      ],
+                                    ],
+                                  ],
+                                );
+                              },
+                            ),
+                          ),
+
+                          // orders
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
-      ),
+            ),
     );
   }
 }
